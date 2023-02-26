ebaac950292c0d29d99e3ebd8c47fb4e68797943
asssemka <113093705+asssemka@users.noreply.github.com>
2023-02-26 22:03:55 +0600

    Add files via upload

---
+++ lab4/mysite/article/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+
+from .views import *
+
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+]
+
+
+
+
---
+++ lab4/mysite/article/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def index(request):
+     return render(request, 'article/index.html')
+
+def about(request):
+     return render(request, 'article/about.html')
+
+
+def categories(request, catid):
+     if(request.GET):
+          print(request.GET)
+
+     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+
+def archive(request, year):
+     if int(year) > 2020:
+          return redirect('/', permanent=True)
+
+     return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
+
+def pageNotFound(request, exception):
+     return HttpResponseNotFound('<h1>Страница не найдена<h1/>')
+
