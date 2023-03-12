ceb68ed8fb16785adf8a3c0e06402501fe16e2be
asssemka <assem.kassenova@narxoz.kz>
2023-03-12 22:33:30 +0600

    lab 6

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -4,10 +4,12 @@ from .views import *
 
 
 urlpatterns = [
+    path('', ArticleHome.as_view(), name='home'),
+    path('addpage/', AddPage.as_view(), name='add_page'),
     path('', index, name='home'),
     path('about/', about, name='about'),
-    path('post/<slug:post_slug>/', show_post, name='post'),
-    path('style/<int:style_id>/', show_category, name='category'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', ArticleCategory.as_view(), name='category'),
 ]
 
 
--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,24 +1,39 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-from article.models import Article, Styles
-
+from .models import *
+from .forms import *
 
 # Create your views here.
 # def index(request):
 #      return render(request, 'article/index.html')
-def index(request):
-    posts = Article.objects.all()
-    styles = Styles.objects.all()
+class ArticleHome(ListView):
+    model = Article
+    template_name = 'article/index.html'
+    context_object_name = 'posts'
 
-    context = {
-        'posts': posts,
-        'styles': styles,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
 
-    return render(request, 'article/index.html', context=context)
+    def get_queryset(self):
+        return Article.objects.filter(is_published=True)
+# def index(request):
+#     posts = Article.objects.all()
+#     styles = Styles.objects.all()
+#
+#     context = {
+#         'posts': posts,
+#         'styles': styles,
+#         'title': 'Главная страница',
+#         'cat_selected': 0,
+#     }
+#
+#     return render(request, 'article/index.html', context=context)
 
 def about(request):
      return render(request, 'article/about.html')
@@ -51,16 +66,66 @@ def show_post(request, post_slug):
 
     return render(request, 'article/post.html', context=context)
 
-def show_category(request, style_id):
-    posts = Article.objects.filter(style_id=style_id)
+class AddPage (CreateView):
+    form_class = AddPostForm
+    template_name = 'article/addpage.html'
+    success_url = reverse_lazy('home')
 
-    if len(posts) == 0:
-        raise Http404()
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи'
+        return context
 
-    context = {
-        'posts': posts,
-        'title': 'Отображение по рубрикам',
-        'cat_selected': style_id,
-    }
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST)
+#         if form.is_valid():
+#             #print(form.cleaned_data)
+#             try:
+#                 Article.objects.create(**form.cleaned_data)
+#                 return redirect('home')
+#             except:
+#                 form.add_error(None, 'Ошибка добавления поста')
+#
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'women/addpage.html', {'form': form, 'title': 'Добавление статьи'})
 
-    return render(request, 'article/index.html', context=context)
+class ArticleCategory(ListView):
+    model = Article
+    template_name = 'article/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Article.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+# def show_category(request, style_id):
+#     posts = Article.objects.filter(style_id=style_id)
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': posts,
+#         'title': 'Отображение по рубрикам',
+#         'cat_selected': style_id,
+#     }
+#
+#     return render(request, 'article/index.html', context=context)
+class ShowPost(DetailView):
+    model = Article
+    template_name = 'article/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        return context
